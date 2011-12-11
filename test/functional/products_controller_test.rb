56c3d05c2a91eca8f58be12212bfc3fee369f895
syghefish <djnaphouse@gmail.com>
2011-12-11 13:09:31 -0500

    added validation

--- test/functional/products_controller_test.rb
+++ test/functional/products_controller_test.rb
@@ -1,49 +1,59 @@
 require 'test_helper'
 
 class ProductsControllerTest < ActionController::TestCase
-  setup do
+    setup do
     @product = products(:one)
-  end
+    @update = {
+    title:       'Lorem Ipsum',
+    description: 'Wibbles are fun!',
+    image_url:   'lorem.jpg',
+    price:       19.95
+    }
+end
 
-  test "should get index" do
+test "should get index" do
     get :index
     assert_response :success
     assert_not_nil assigns(:products)
-  end
+end
 
-  test "should get new" do
+test "should get new" do
     get :new
     assert_response :success
-  end
+end
 
-  test "should create product" do
+test "should create product" do
     assert_difference('Product.count') do
-      post :create, product: @product.attributes
+        post :create, product: @update
     end
-
+    
     assert_redirected_to product_path(assigns(:product))
-  end
+end
 
-  test "should show product" do
+# ...
+
+test "should show product" do
     get :show, id: @product.to_param
     assert_response :success
-  end
+end
 
-  test "should get edit" do
+test "should get edit" do
     get :edit, id: @product.to_param
     assert_response :success
-  end
+end
 
-  test "should update product" do
-    put :update, id: @product.to_param, product: @product.attributes
+test "should update product" do
+    put :update, id: @product.to_param, product: @update
     assert_redirected_to product_path(assigns(:product))
-  end
+end
 
-  test "should destroy product" do
+# ...
+
+test "should destroy product" do
     assert_difference('Product.count', -1) do
-      delete :destroy, id: @product.to_param
+        delete :destroy, id: @product.to_param
     end
-
+    
     assert_redirected_to products_path
-  end
+end
 end
