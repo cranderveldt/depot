05fab93467274f74fdcdb07f43509b89d2da5ef6
syghefish <djnaphouse@gmail.com>
2011-12-27 21:49:07 -0500

    error handling cart

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111228021039) do
+ActiveRecord::Schema.define(:version => 20111228022147) do
 
   create_table "carts", :force => true do |t|
     t.datetime "created_at"
